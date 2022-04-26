b67d95e61430757f5bb828bf88c5e61c14b26b30
Divyansh Mishra <divyanshm471@gmail.com>
2022-04-26 15:23:27 +0530

    REST API for goals created

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,14 @@
 //server.js = entry point to our server
 const express = require('express');
+const colors = require('colors');
 const { rmSync } = require('fs');
 const { get } = require('http');
 const dotenv = require('dotenv').config();
+const connectDB = require('./config/db')
 const port = process.env.PORT || 5000;
 const {errorHandler} = require('./middleware/errorHandler')
 
+connectDB()
 const app = express();
 
 app.use(express.json())
@@ -13,4 +16,4 @@ app.use(express.urlencoded({extended: false}))
 
 app.use('/api/goals', require('./routes/goalRoutes'));
 app.use(errorHandler)
-app.listen(port, () => console.log(`Server started on port ${port}`))
+app.listen(port, () => console.log(`Server started on port ${port}`.green))
