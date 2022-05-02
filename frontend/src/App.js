1d2d931fef7a00d112d6a51d666be6a66af6f6eb
Divyansh Mishra <divyanshm471@gmail.com>
2022-05-02 11:32:36 +0530

    ADDED FRONTEND UI

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,23 @@
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./Components/Header";
+import Dashboard from "./Pages/Dashboard";
+import Login from "./Pages/Login";
+import Register from "./Pages/Register";
 function App() {
-  return (
-    <div>
-      <h1>My Application</h1>
+ return (
+  <>
+   <Router>
+    <div className="container">
+      <Header />
+      <Routes>
+        <Route path = '/' element={<Dashboard />} />
+        <Route path = '/login' element={<Login />} />
+        <Route path = '/register' element={<Register />} />
+      </Routes>
     </div>
-  );
+   </Router>
+  </>
+ );
 }
 
 export default App;
