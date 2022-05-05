6d7ec81795b8e07ce0a6c9bb9817749f745d09ab
Divyansh Mishra <divyanshm471@gmail.com>
2022-05-05 16:14:48 +0530

    prepare deploy

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 //server.js = entry point to our server
+const path = require('path')
 const express = require('express');
 const colors = require('colors');
 const { rmSync } = require('fs');
@@ -6,7 +7,8 @@ const { get } = require('http');
 const dotenv = require('dotenv').config();
 const connectDB = require('./config/db')
 const port = process.env.PORT || 5000;
-const {errorHandler} = require('./middleware/errorHandler')
+const {errorHandler} = require('./middleware/errorHandler');
+const res = require('express/lib/response');
 connectDB()
 const app = express();
 
@@ -15,5 +17,14 @@ app.use(express.urlencoded({extended: false}))
 
 app.use('/api/goals', require('./routes/goalRoutes'));
 app.use('/api/user', require('./routes/userRoutes'));
+
+//serve frontend
+if(process.env.NODE_ENV === 'production'){
+  app.use(express.static(path.join(__dirname, '../frontend/build')))
+
+  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')))
+}else{
+  app.get('/', (req, res) => res.send('Please set to production'))
+}
 app.use(errorHandler)
 app.listen(port, () => console.log(`Server started on port ${port}`.green))
