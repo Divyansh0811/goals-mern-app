a6db00d39d242eb90e57ea80baf24a6ea3e3571a
Divyansh Mishra <divyanshm471@gmail.com>
2023-02-23 17:38:49 +0530

    cors added

--- backend/server.js
+++ backend/server.js
@@ -9,9 +9,10 @@ const connectDB = require('./config/db')
 const port = process.env.PORT || 5000;
 const {errorHandler} = require('./middleware/errorHandler');
 const res = require('express/lib/response');
+const cors = require('cors');
 connectDB()
 const app = express();
-
+app.use(cors());
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
